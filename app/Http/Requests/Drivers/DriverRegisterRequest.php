4985886c59578ec6ab46cc91657b299a0deda1a7
Caio Vinicius Albertinazi Mizuno <caiomizuninho@gmail.com>
2023-02-02 21:24:03 -0400

    request de registro

---
+++ app/Http/Requests/Drivers/DriverRegisterRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Drivers;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DriverRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'phone_number'       => 'required',
+            'password'           => 'required',
+
+            'first_name'         => 'required',
+            'last_name'          => 'nullable',
+            'email'              => 'required',
+            'gender'             => 'required',
+            'birth_date'         => 'required',
+            'license'            => 'required',
+            'dt_license_expired' => 'required',
+
+            "plate"      => 'required',
+            "year"       => 'nullable',
+            "model"      => 'nullable',
+            "brand"      => 'nullable',
+            "color"      => 'nullable',
+            "renavam"    => 'nullable',
+            "ride_model" => 'required',
+        ];
+    }
+}
