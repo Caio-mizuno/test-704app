d6927753236b3822b5f33fffa243e159d15fb238
Caio Vinicius Albertinazi Mizuno <caiomizuninho@gmail.com>
2023-02-02 21:56:59 -0400

    fix - configurações para a aplicação rodar

--- routes/drivers.php
+++ routes/drivers.php
@@ -1,4 +1,8 @@
 <?php
+
+use App\Http\Controllers\Auth\LoginDriversController;
+use App\Http\Controllers\Auth\RegisterDriversController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('login', DriverLoginController::class);
-Route::post('register', DriverRegisterController::class);
+Route::post('login', LoginDriversController::class);
+Route::post('register', RegisterDriversController::class);
 
 Route::middleware(['driver.token'])->group(function () {
 });
