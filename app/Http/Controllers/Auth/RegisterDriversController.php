d91c08154fce9ea2522c014db52296c5e049e251
Caio Vinicius Albertinazi Mizuno <caiomizuninho@gmail.com>
2023-02-02 21:57:29 -0400

    add - escopo de registro de motorista

--- app/Http/Controllers/Auth/RegisterDriversController.php
+++ app/Http/Controllers/Auth/RegisterDriversController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Drivers\DriverLoginRequest;
+use App\Http\Requests\Drivers\DriverRegisterRequest;
+use App\Repository\Driver\DriverRepository;
 use Illuminate\Http\Request;
 
 class RegisterDriversController extends Controller
 {
-    //
+    private $driverRepo;
+
+    public function __construct(DriverRepository $driver){
+        $this->driverRepo = $driver;
+    }
+
+    public function __invoke(DriverRegisterRequest $request){
+        $data = $request->all();
+        
+        $driver = $this->driverRepo->register($request->all());
+        
+        if($driver){
+
+            return $this->responseSuccess(
+                array('driver' => $driver,)
+            );
+        }
+        else{
+            return  $this->responseError([
+                'error' => "Problemas ao encontrar Motorista. Telefone ou senha podem estar inválidos!"
+            ]);
+        }
+    }
 }
