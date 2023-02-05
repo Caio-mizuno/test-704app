d5c5dbfa1580db8b4d004ced87b1630efb3e2d56
Caio Vinicius Albertinazi Mizuno <caiomizuninho@gmail.com>
2023-02-05 02:51:33 -0400

    add - login de usuários

---
+++ app/Http/Controllers/Auth/LoginUsersController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Users\UserLoginRequest;
+use App\Repository\User\UserRepository;
+use Illuminate\Http\Request;
+
+class LoginUsersController extends Controller
+{
+    private $userRepo;
+
+    public function __construct(UserRepository $user){
+        $this->userRepo = $user;
+    }
+    
+    public function __invoke(UserLoginRequest $request){
+        $data = $request->all();
+        $user = $this->userRepo->login($data['email'], $data['password']);
+        if($user){
+
+            $token = MakeJwt([
+                'data' => [
+                    'iss' => 'mooveapp',
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'time' => 'today'.time()
+                ]
+            ]);
+            $user->access_token = $token;
+            $user->save();
+            
+            //Criando registro de login com Token de expiração
+            $this->userRepo->personalAccessCreate($user, $token);
+            
+            return  $this->responseSuccess(
+                array(
+                    'user'      => $user,
+                    'error'     => null,
+                    'token'     => $token,
+                ));
+        }
+        else{
+            return  $this->responseError([
+                'error' => "Problemas ao encontrar Usuário. Telefone ou senha podem estar inválidos!"
+            ]);
+        }
+    }
+}
