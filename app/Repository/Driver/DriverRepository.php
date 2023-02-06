887a55cbcf8aeefc9f55697192b60000cc77a37c
Caio Vinicius Albertinazi Mizuno <caiomizuninho@gmail.com>
2023-02-06 10:54:21 -0400

    add - regra de negócio pra pesquisar motorista

--- app/Repository/Driver/DriverRepository.php
+++ app/Repository/Driver/DriverRepository.php
@@ -24,27 +24,25 @@ class DriverRepository extends BaseRepository implements EntityRepositoryInterfa
     public function checkToken(string $token): bool
     {
         $driver = $this->model->newQuery()
-            ->where('access_token','=', $token)
+            ->where('access_token', '=', $token)
             ->first();
-            if($driver){
-                $personal_token = AccessToken::
-                where('entity','=', 'driver')
-                ->where('entity_id','=',$driver->id)
-                ->where('token','=',$token)
+        if ($driver) {
+            $personal_token = AccessToken::where('entity', '=', 'driver')
+                ->where('entity_id', '=', $driver->id)
+                ->where('token', '=', $token)
                 ->first();
-            if($personal_token){
+            if ($personal_token) {
                 $now = date('Y-m-d H:i:s');
-                if($personal_token->expires_at < $now){
+                if ($personal_token->expires_at < $now) {
                     return false;
                 }
                 //Atualizando a ultima data de acesso
-                $personal_token->last_used_at = date('Y-m-d H:i:s',time());
+                $personal_token->last_used_at = date('Y-m-d H:i:s', time());
                 $personal_token->save();
                 return true;
-            }else
+            } else
                 return false;
-        }
-        else
+        } else
             return false;
     }
 
@@ -55,12 +53,12 @@ class DriverRepository extends BaseRepository implements EntityRepositoryInterfa
             ->first();
 
         if (!empty($driver)) {
-            
+
             //Verificação de senha conforme o tipo de Password
-            if(!Hash::check($password,$driver->password)){
+            if (!Hash::check($password, $driver->password)) {
                 return 'Senha incorreta';
             }
-           
+
 
             //Verificando se o motorista está bloqueado
             if ($driver->is_blocked == true) {
@@ -72,8 +70,6 @@ class DriverRepository extends BaseRepository implements EntityRepositoryInterfa
                     $driver->save();
                 }
             }
-
-            
         }
         unset($driver->password);
         return $driver;
@@ -84,23 +80,23 @@ class DriverRepository extends BaseRepository implements EntityRepositoryInterfa
             ->where('phone_number', $phone_number)
             ->exists();
     }
-   
+
     public function register(array $request)
     {
         //tratando o numero do telefone
         $request['phone_number'] =  preg_replace('/[\(\)\-\s]/', '', $request['phone_number']);
         $check = $this->checkPhoneExists($request['phone_number']);
         if ($check == false) {
-            
+
 
             DB::beginTransaction();
             try {
 
                 $vehicleRepo = new VehicleRepository(new Vehicle);
                 $new_vehicle = $vehicleRepo->createVehicle($request);
-                
+
                 $data = [
-                    'vehicle_id'=>$new_vehicle->id,
+                    'vehicle_id' => $new_vehicle->id,
                     'first_name' => $request['first_name'],
                     'last_name' => $request['last_name'],
                     'payment_style' => json_encode(["money", "credit_card"]),
@@ -113,33 +109,60 @@ class DriverRepository extends BaseRepository implements EntityRepositoryInterfa
 
                 $new = $this->model->newQuery()->create($data);
                 DB::commit();
-                
+
                 return $new;
             } catch (Exception $e) {
                 DB::rollBack();
                 return $e->getMessage();
             }
-            
         }
         return 'Número de Telefone já existe';
     }
-    public function personalAccessCreate($driver,$token){
+    public function personalAccessCreate($driver, $token)
+    {
         $personal_access = AccessToken::where('entity', 'driver')->where('entity_id', $driver->id)->first();
-        if($personal_access){
+        if ($personal_access) {
             $personal_access->update([
-                'token'=>$token,
-                'expires_at' => date('Y-m-d',strtotime('+1 DAY'))
+                'token' => $token,
+                'expires_at' => date('Y-m-d', strtotime('+1 DAY'))
             ]);
-
-        }else{
+        } else {
             //criando registro de token
             $personal_access = AccessToken::create([
-                'token'=>$token,
+                'token' => $token,
                 'entity' => AccessTokenEnum::Driver,
                 'entity_id' => $driver->id,
-                'expires_at' => date('Y-m-d',strtotime('+1 DAY'))
+                'expires_at' => date('Y-m-d', strtotime('+1 DAY'))
             ]);
         }
         return $personal_access->token;
+    }
+
+    public function searchDriver(array $filter)
+    {
+        $list = Driver::query();
+        $list->join('vehicles', 'vehicles.id', 'drivers.vehicle_id');
+
+        //Query name
+        if (isset($filter['first_name'])) {
+            $list->where('first_name', 'LIKE', '%' . $filter['first_name'] . '%');
+        }
+
+        //Query plate
+        if (isset($filter['plate'])) {
+            $list->where('plate', 'LIKE', '%' . $filter['plate'] . '%');
+        }
+
+        //Query license
+        if (isset($filter['license'])) {
+            $list->where('license', 'LIKE', '%' . $filter['license'] . '%');
+        }
+
+         //Query license
+         if (isset($filter['cpf'])) {
+            $list->where('cpf', 'LIKE', '%' . $filter['cpf'] . '%');
+        }
+
+        return $list->simplePaginate();
     }
 }
